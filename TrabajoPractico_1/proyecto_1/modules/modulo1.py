b429623daa8da3378c0a77a8fee90ff0edb4857e
Natymicheloud <natymiche2@gmail.com>
2025-03-14 16:27:44 -0300

    modulo1.py

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,2 +1,5 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
+
+def for i in range(10):
+  print("i")
